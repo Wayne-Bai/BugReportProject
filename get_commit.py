51207ad322f2d0e4ae1c86964702f9a4c37c98a7
wayne <bwh.buaa@gmail.com>
2022-03-19 22:56:32 -0700

    add date,email,committer info

--- get_commit.py
+++ get_commit.py
@@ -3,7 +3,7 @@ import json
 
 repo = Repo('linux')
 
-print(repo.head.commit)
+# print(repo.head.commit)
 
 count1 = 0
 count2 = 0
@@ -23,10 +23,15 @@ with open('commit_git.json','a') as f:
                 and 'comment' not in str(commit.summary).lower() and date-2017 >= 0:
         # if 'fix' in str(commit.summary).lower() and 'merge' not in str(commit.summary).lower():
             temp['ID'] = commit.hexsha
-            temp['DATE'] = str(commit.authored_datetime)
-            # print(str(commit.authored_datetime))
+            temp['AUTHORED_DATE'] = str(commit.authored_datetime)
             temp['AUTHOR'] = str(commit.author)
+            temp['AUTHOR_EMAIL'] = str(commit.author.email)
+            temp['COMMITTED_DATE'] = str(commit.committed_datetime)
+            # print(str(commit.authored_datetime))
+            temp['COMMITTER'] = str(commit.committer)
+            temp['COMMITTER_EMAIL'] = str(commit.committer.email)
             temp['SUMMARY'] = str(commit.summary)
+            temp['SIZE'] = commit.size
             # print(commit.summary)
             temp['MESSAGE'] = str(commit.message)
 
