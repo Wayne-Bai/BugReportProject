712b943f60f7ab8f7bc077dbdb203acc50025440
wayne <bwh.buaa@gmail.com>
2022-03-20 18:16:04 -0700

    get email containing 'fix'

---
+++ get_fix_email.py
@@ -0,0 +1,37 @@
+import json
+
+# Delete 'no-fix' data
+
+count = 0
+fix_email = []
+
+with open('../2022_email_v1.json','r') as f:
+    for line in f.readlines():
+        data = json.loads(line)
+        if 'fix' in str(data['subject']).lower().split() and 'merge' not in str(data['subject']).lower() \
+                and 'performance' not in str(data['subject']).lower() and 'perform' not in str(data['subject']).lower() \
+                and 'performing' not in str(data['subject']).lower() and 'typo' not in str(data['subject']).lower() \
+                and 'revert' not in str(data['subject']).lower() and 'spelling' not in str(data['subject']).lower() \
+                and 'readability' not in str(data['subject']).lower() and 'documentation' not in str(data['subject']).lower() \
+                and 'comment' not in str(data['subject']).lower():
+            fix_email.append(data)
+f.close()
+
+with open('temp_fix_total.json','a') as w:
+    for i in fix_email:
+        w.write(json.dumps(i))
+        w.write('\n')
+w.close()
+
+#Generate Conversation
+# total_conversation = []
+# for i in range(len(fix_email)):
+#     if 'Re:' not in fix_email[i]['subject']:
+#         temp = []
+#         temp.append(fix_email[i])
+#         for j in range(i+1, len(fix_email)):
+#             if fix_email[i]['subject'] in fix_email[j]['subject']:
+#                 temp.append(fix_email[j])
+#         total_conversation.append(temp)
+# print(len(total_conversation))
+
