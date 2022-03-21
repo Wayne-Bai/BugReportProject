86008fde9a5324e79547fb6df418d738ff590dc1
wayne <bwh.buaa@gmail.com>
2022-03-21 09:06:58 -0700

    add map conversation to authors

--- get_conversation.py
+++ get_conversation.py
@@ -2,11 +2,13 @@ import json
 
 total_email = []
 
-with open("temp_fix_total.json",'r') as f:
+with open("2022_fix_total.json",'r') as f:
     for line in f.readlines():
         data = json.loads(line)
         total_email.append(data)
 f.close()
+
+print(len(total_email))
 
 total_conversation = []
 for i in range(len(total_email)):
@@ -16,5 +18,29 @@ for i in range(len(total_email)):
         for j in range(i+1, len(total_email)):
             if total_email[i]['subject'][0] in total_email[j]['subject'][0]:
                 temp.append(total_email[j])
-        print(temp)
+        # print(temp)
         total_conversation.append(temp)
+
+print(len(total_conversation))
+
+# map to same author
+
+email_map = {}
+for email in total_conversation:
+    first_email = email[0]
+    if first_email['author']:
+        author = first_email['author'][0]
+    else:
+        author = 'N/A'
+    if author not in email_map.keys():
+        email_map[author] = []
+        email_map[author].append(email)
+    else:
+        email_map[author].append(email)
+
+print(len(email_map.keys()))
+
+for key,value in email_map.items():
+    print("Author: {}, Submit Number: {}".format(key, len(value)))
+
+# print(email_map.keys())
