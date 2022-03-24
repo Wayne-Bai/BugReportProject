34512f5402897de59d67c4225a89ee9bb371e30a
wayne <bwh.buaa@gmail.com>
2022-03-24 11:30:22 -0700

    get email number

---
+++ check_file.py
@@ -0,0 +1,15 @@
+import json
+
+data_list = ['2017', '2018', '2019', '2021', '2022']
+
+count = 0
+
+for i in data_list:
+
+    with open(i+'_email.json', 'r') as f:
+        for line in f.readlines():
+            count += 1
+
+    print('{}: {}'.format(i,count))
+
+    f.close()
