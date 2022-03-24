4a970cbb1f76686fcc4949f46c59222182e11e94
wayne <bwh.buaa@gmail.com>
2022-03-24 12:04:36 -0700

    add check the commit

--- check_file.py
+++ check_file.py
@@ -1,15 +1,35 @@
 import json
 
-data_list = ['2017', '2018', '2019', '2021', '2022']
+file_type = 'commit'
 
-count = 0
+if file_type == 'email':
 
-for i in data_list:
+    data_list = ['2017', '2018', '2019', '2021', '2022']
 
-    with open('dataset/' + i+'_email.json', 'r') as f:
-        for line in f.readlines():
-            count += 1
+    count = 0
 
-    print('{}: {}'.format(i,count))
+    for i in data_list:
 
-    f.close()
+        with open('dataset/' + i+'_email.json', 'r') as f:
+            for line in f.readlines():
+                count += 1
+
+        print('{}: {}'.format(i,count))
+
+        f.close()
+
+elif file_type == 'commit':
+
+    data_list = ['2017', '2018', '2019', '2020', '2021', '2022']
+
+    count = 0
+
+    for i in data_list:
+
+        with open(i + '_commit.json', 'r') as f:
+            for line in f.readlines():
+                count += 1
+
+        print('{}: {}'.format(i, count))
+
+        f.close()
