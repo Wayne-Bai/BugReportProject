d5c4f0ad4393d12ec1d8b71ff7fef7d96bd434b0
wayne <bwh.buaa@gmail.com>
2022-03-27 21:32:28 -0700

    check

--- check_email_number.py
+++ check_email_number.py
@@ -1,31 +1,35 @@
 import json
 
-with open('fix_total.json', 'r') as f:
+with open('whole_email.json', 'r') as f:
     num_2017 = 0
     num_2018 = 0
     num_2019 = 0
     num_2020 = 0
     num_2021 = 0
     num_2022 = 0
+    num_undefine = 0
     for line in f.readlines():
         data = json.loads(line)
-        if data['date'][0].split()[3] == '2017':
-            num_2017 += 1
-        elif data['date'][0].split()[3] == '2018':
-            num_2018 += 1
-        elif data['date'][0].split()[3] == '2019':
-            num_2019 += 1
-        elif data['date'][0].split()[3] == '2020':
-            num_2020 += 1
-        elif data['date'][0].split()[3] == '2021':
-            num_2021 += 1
-        elif data['date'][0].split()[3] == '2022':
-            num_2022 += 1
-
+        if data['date']:
+            if data['date'][0].split()[3] == '2017':
+                num_2017 += 1
+            elif data['date'][0].split()[3] == '2018':
+                num_2018 += 1
+            elif data['date'][0].split()[3] == '2019':
+                num_2019 += 1
+            elif data['date'][0].split()[3] == '2020':
+                num_2020 += 1
+            elif data['date'][0].split()[3] == '2021':
+                num_2021 += 1
+            elif data['date'][0].split()[3] == '2022':
+                num_2022 += 1
+        else:
+            num_undefine += 1
     print('2017: {}'.format(num_2017))
     print('2018: {}'.format(num_2018))
     print('2019: {}'.format(num_2019))
     print('2020: {}'.format(num_2020))
     print('2021: {}'.format(num_2021))
     print('2022: {}'.format(num_2022))
+    print('undefine: {}'.format(num_undefine))
 f.close()
