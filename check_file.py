d42277ef6ca54b332b801ea7435931a8f297fafd
wayne <bwh.buaa@gmail.com>
2022-03-29 14:33:58 -0700

    x

--- check_file.py
+++ check_file.py
@@ -1,14 +1,16 @@
 import json
 
-file_type = 'commit'
+file_type = 'email'
 
 if file_type == 'email':
 
     data_list = ['2017', '2018', '2019', '2021', '2022']
 
-    count = 0
+
 
     for i in data_list:
+
+        count = 0
 
         with open('dataset/' + i+'_email.json', 'r') as f:
             for line in f.readlines():
